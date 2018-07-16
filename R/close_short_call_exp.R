57979ce05daa50286302825850034d105aafec4c
Jason <jtaylor@discounttire.com>
2018-07-16 10:01:46 -0700

    add close_short_call_exp function

---
+++ R/close_short_call_exp.R
@@ -0,0 +1,15 @@
+#' @export
+close_short_call_exp <- function(df, date, exp, c_strike, credit) {
+  closes <- df %>%
+    dplyr::filter(expiration == exp, quotedate == exp,
+                  strike == c_strike & type == "call") %>%
+    dplyr::group_by(quotedate) %>%
+    dplyr::mutate(open_date = as.Date(date, origin = "1970-01-01"),
+                  open_credit = credit,
+                  debit = sum(mid),
+                  profit = open_credit - debit) %>%
+    dplyr::ungroup() %>%
+    dplyr::select(symbol, quotedate, expiration, open_date,
+                  open_credit, debit, profit) %>%
+    dplyr::distinct()
+}
