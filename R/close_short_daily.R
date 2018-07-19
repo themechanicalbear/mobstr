0d10eb8409e61b2b96f70478befc15be0250c2ef
Jason Taylor <jason@themechanicalbear.com>
2018-07-18 19:29:59 -0700

    strike parameter to strk

--- R/close_short_daily.R
+++ R/close_short_daily.R
@@ -1,12 +1,14 @@
 #' @export
-close_short_daily <- function(df, date, exp, strike, credit, put_call) {
+close_short_daily <- function(df, date, exp, strk, credit, put_call) {
   closes <- df %>%
-    dplyr::filter(expiration == exp, quotedate > date,
-                  strike == strike & type == put_call) %>%
+    dplyr::filter(expiration == exp,
+                  quotedate > date,
+                  strike == strk,
+                  type == put_call) %>%
     dplyr::group_by(quotedate) %>%
     dplyr::mutate(open_date = as.Date(date, origin = "1970-01-01"),
                   open_credit = credit,
-                  debit = sum(mid),
+                  debit = mid,
                   profit = open_credit - debit) %>%
     dplyr::ungroup() %>%
     dplyr::select(symbol, quotedate, expiration, open_date,
