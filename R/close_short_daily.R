d74adabf45ba33321db62c8470c4694d1f2f51b9
Jason <jason@themechanicalbear.com>
2018-08-03 10:45:28 -0700

    close_short_daily function now uses !! variable inputs/names

--- R/close_short_daily.R
+++ R/close_short_daily.R
@@ -1,17 +1,27 @@
 #' @export
 close_short_daily <- function(df, date, exp, strk, credit, put_call) {
-  closes <- df %>%
+  open_credit <- paste0(put_call, "_open_credit")
+  close_debit <- paste0(put_call, "_close_debit")
+  profit <- paste0(put_call, "_profit")
+
+  df %>%
     dplyr::filter(expiration == exp,
                   quotedate > date,
                   strike == strk,
                   type == put_call) %>%
     dplyr::group_by(quotedate) %>%
-    dplyr::mutate(open_date = as.Date(date, origin = "1970-01-01"),
-                  open_credit = credit,
-                  debit = mid,
-                  profit = open_credit - debit) %>%
+    dplyr::mutate(open_date = date,
+                  open_credit := credit,
+                  close_debit := mid,
+                  profit := credit - mid) %>%
     dplyr::ungroup() %>%
+    dplyr::collect() %>%
+    dplyr::mutate(open_date = as.Date(open_date, origin = "1970-01-01"),
+                  expiration = as.Date(expiration, origin = "1970-01-01"),
+                  quotedate = as.Date(quotedate, origin = "1970-01-01"),
+                  !!open_credit := open_credit,
+                  !!close_debit := close_debit,
+                  !!profit := profit) %>%
     dplyr::select(symbol, quotedate, expiration, open_date,
-                  open_credit, debit, profit) %>%
-    dplyr::distinct()
+                  !!open_credit, !!close_debit, !!profit)
 }
