fd6b641eeadde474abf163ccbc718f6866e3e67b
Jason <jason@themechanicalbear.com>
2018-08-03 10:42:42 -0700

    open_short function now uses !! variable inputs/names

--- R/open_short.R
+++ R/open_short.R
@@ -1,12 +1,22 @@
 #' @export
 open_short <- function(data, tar_delta, put_call) {
+  tp <- paste0(put_call, "_type")
+  s <- paste0(put_call, "_strike")
+  ds <- paste0(put_call, "_delta_strike")
+  oc <- paste0(put_call, "_open_credit")
+
   data %>%
     dplyr::filter(type == put_call) %>%
-    dplyr::mutate(abs_delta = abs(delta - tar_delta)) %>%
+    dplyr::mutate(abs_delta = abs(delta_strike - tar_delta)) %>%
     dplyr::group_by(quotedate) %>%
-    dplyr::filter(m_dte == min(m_dte)) %>%
-    dplyr::filter(abs_delta == min(abs_delta)) %>%
-    dplyr::filter(dplyr::row_number() == 1) %>%
+    dplyr::filter(m_dte == min(m_dte, na.rm = TRUE)) %>%
+    dplyr::filter(abs_delta == min(abs_delta, na.rm = TRUE)) %>%
     dplyr::ungroup() %>%
-    dplyr::select(quotedate, type, expiration, strike, delta, dte, mid)
+    dplyr::collect() %>%
+    dplyr::mutate(!!tp := type,
+                  !!s := strike,
+                  !!ds := delta_strike,
+                  !!oc := mid) %>%
+    dplyr::select(quotedate, !!tp, expiration, !!s, !!ds, dte, !!oc)
 }
+
