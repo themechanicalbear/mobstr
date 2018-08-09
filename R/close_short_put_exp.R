022ee51c5e24a5ba6808d6c9616a2bcce35cc856
Jason <jason@themechanicalbear.com>
2018-08-08 20:16:46 -0700

    update close_short_put_exp will probably remove soon for more generic function

--- R/close_short_put_exp.R
+++ R/close_short_put_exp.R
@@ -1,15 +1,15 @@
 #' @export
 close_short_put_exp <- function(df, date, exp, p_strike, credit) {
-  closes <- df %>%
+  df %>%
     dplyr::filter(expiration == exp, quotedate == exp,
                   strike == p_strike & type == "put") %>%
     dplyr::group_by(quotedate) %>%
-    dplyr::mutate(open_date = as.Date(date, origin = "1970-01-01"),
-                  open_credit = credit,
-                  debit = sum(mid),
+    dplyr::mutate(open_credit = credit,
+                  debit = sum(mid, na.rm = TRUE),
                   profit = open_credit - debit) %>%
     dplyr::ungroup() %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(open_date = as.Date(date, origin = "1970-01-01")) %>%
     dplyr::select(symbol, quotedate, expiration, open_date,
-                  open_credit, debit, profit) %>%
-    dplyr::distinct()
+                  open_credit, debit, profit)
 }
