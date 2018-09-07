38819d59da79dd7f0ce40d1de4eeb71378bd4fb4
Jason <jason@themechanicalbear.com>
2018-09-07 08:01:23 -0700

    reorder redshift connection, add todo for s3 bucket creation

--- R/load_redshift.R
+++ R/load_redshift.R
@@ -1,11 +1,14 @@
 #' @export
 load_redshift <- function(stock) {
-  rs_conn <- tastytrade::redshift_connect("TASTYTRADE")
-
   options <- readRDS(paste0(here::here(), "/data/options/", stock, ".RDS")) %>%
     dplyr::mutate(mid = (bid + ask) / 2)
 
+  # S3 bucket must already exist at this point.
+  # TODO - Add code to create the bucket if not exists
   tastytrade::write_2_S3(options, paste0(stock, "_options.csv"), paste0(stock, "-options"))
+
+  # Connect to Redshift
+  rs_conn <- tastytrade::redshift_connect("TASTYTRADE")
 
   if (!RJDBC::dbExistsTable(rs_conn, stock)) {
     RJDBC::dbSendUpdate(rs_conn, paste0("create table if not exists ", stock, "(
@@ -44,5 +47,6 @@ load_redshift <- function(stock) {
       sortkey (symbol, quotedate);"))
   }
   tastytrade::truncate_redshift(rs_conn, stock)
+  # Use Manage IAM roles on cluster to add the redshift role prior to copy
   tastytrade::copy_S3_redshift(rs_conn, stock, paste0("s3://", stock, "-options/"))
 }
