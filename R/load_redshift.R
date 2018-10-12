37f4f255995171460476ac34e790762d0c76a155
Jason Taylor <jason@themechanicalbear.com>
2018-10-12 16:46:36 -0700

    load_redshift updates

--- R/load_redshift.R
+++ R/load_redshift.R
@@ -1,18 +1,11 @@
 #' @export
 load_redshift <- function(stock) {
-  options <- readRDS(paste0(here::here(), "/data/options/", stock, ".RDS")) %>%
-    dplyr::mutate(mid = (bid + ask) / 2)
-
-  # S3 bucket must already exist at this point.
-  # TODO - Add code to create the bucket if not exists
-  tastytrade::write_2_S3(options, paste0(stock, "_options.csv"), paste0(stock, "-options"))
-
-  # Connect to Redshift
   rs_conn <- tastytrade::redshift_connect("TASTYTRADE")
 
   if (!RJDBC::dbExistsTable(rs_conn, stock)) {
-    RJDBC::dbSendUpdate(rs_conn, paste0("create table if not exists ", stock, "(
-      symbol varchar(4) not null,
+    RJDBC::dbSendUpdate(rs_conn,
+                        paste0("create table if not exists ", stock, "(
+      symbol varchar(5) not null,
       quotedate date,
       calliv float(4),
       putiv float(4),
@@ -48,5 +41,7 @@ load_redshift <- function(stock) {
   }
   tastytrade::truncate_redshift(rs_conn, stock)
   # Use Manage IAM roles on cluster to add the redshift role prior to copy
-  tastytrade::copy_S3_redshift(rs_conn, stock, paste0("s3://", stock, "-options/"))
+  tastytrade::copy_S3_redshift(env = "TASTYTRADE", connection = rs_conn,
+                               table_name = stock,
+                               bucket_path = paste0("s3://rds-options-files/", stock, "_options.csv"))
 }
