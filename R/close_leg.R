427748e2d7b284e2db3f53f85a85b25428317042
Jason Taylor <jason@themechanicalbear.com>
2018-12-28 18:46:07 -0700

    update roxygen

--- R/close_leg.R
+++ R/close_leg.R
@@ -18,17 +18,12 @@
 #' @param stk_price stock price when position was opened
 #' @param direction was this a short or long position used to calc profit
 #'
-#' @export
-#'
-#'
 #' @return A dataframe of filtered closing option trades to be used in further
 #' analysis. One row for each day a posistion was open open_date +1 through
 #' expiration
 #'
-#' @examples
+#' @export
 #'
-#'
-# Function ----
 close_leg <- function(df, entry_date, exp, typ, stk,
                       entry_mid, entry_delta, stk_price, direction) {
   org <- "1970-01-01"
@@ -38,23 +33,23 @@ close_leg <- function(df, entry_date, exp, typ, stk,
   tdel <- paste0(typ, "_entry_delta")
 
   df %>%
-    filter(expiration == exp,
-           quotedate > entry_date,
-           strike == stk,
-           type == typ) %>%
+    dplyr::filter(.data$expiration == exp,
+                  .data$quotedate > entry_date,
+                  .data$strike == stk,
+                  .data$type == typ) %>%
     collect() %>%
-    mutate(direction = direction) %>%
-    mutate(exit_mid = case_when(direction == "short" ~ mid,
-                                TRUE ~ -mid),
-           profit = entry_mid - exit_mid,
-           entry_date = as.Date(entry_date, origin = org),
-           quotedate = as.Date(quotedate, origin = org),
-           expiration = as.Date(expiration, origin = org),
-           entry_stock_price = stk_price,
-           !!tentm := entry_mid,
-           !!textm := exit_mid,
-           !!tpro := profit,
-           !!tdel := entry_delta) %>%
-    select(symbol, quotedate, expiration, entry_date, entry_stock_price,
-           !!tdel, !!tentm, !!textm, !!tpro)
+    dplyr::mutate(direction = direction) %>%
+    dplyr::mutate(exit_mid = case_when(direction == "short" ~ mid,
+                                       TRUE ~ -mid),
+                  profit = entry_mid - .data$exit_mid,
+                  entry_date = as.Date(.data$entry_date, origin = org),
+                  quotedate = as.Date(.data$quotedate, origin = org),
+                  expiration = as.Date(.data$expiration, origin = org),
+                  entry_stock_price = stk_price,
+                  !!tentm := entry_mid,
+                  !!textm := .data$exit_mid,
+                  !!tpro := .data$profit,
+                  !!tdel := entry_delta) %>%
+    dplyr::select(.data$symbol, .data$quotedate, .data$expiration, .data$entry_date,
+                  .data$entry_stock_price, !!tdel, !!tentm, !!textm, !!tpro)
 }
