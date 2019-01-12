c26c44bdb151a91e71a894b9d8a558c680048ae0
Jason <jason@themechanicalbear.com>
2019-01-12 09:25:55 -0700

    add create bucket code

--- R/write_2_s3.R
+++ R/write_2_s3.R
@@ -7,8 +7,10 @@
 #'
 #' @importFrom dplyr mutate
 #' @importFrom utils write.csv
-#' @importFrom aws.s3 put_object
+#' @importFrom aws.s3 put_object save_object put_bucket
 #' @importFrom here here
+#' @importFrom data.table fread
+#' @importFrom readr write_csv
 #'
 #' @param stock character string of the stock we want to put to S3
 #'
@@ -16,19 +18,41 @@
 #'
 #' @export
 #'
+# write_2_S3 <- function(stock) {
+#   file_name <- paste0(stock, "_options.csv")
+#   bucket_name <- "rds-options-files"
+#   region_name <- Sys.getenv("AWS_S3_REGION")
+#
+#   df <- readRDS(paste0(here(), "/data/options/", stock, ".RDS")) %>%
+#     mutate(mid = (.data$bid + .data$ask) / 2)
+#
+#   tmp <- tempfile()
+#   on.exit(unlink(tmp))
+#   write.csv(df, file = tmp, row.names = FALSE)
+#   # S3 bucket must already exist at this point
+#   put_object(tmp,
+#              object = file_name,
+#              bucket = bucket_name,
+#              region = region_name)
+# }
+
+
 write_2_S3 <- function(stock) {
   file_name <- paste0(stock, "_options.csv")
-  bucket_name <- "rds-options-files"
+
+  aws.s3::save_object(paste0("s3://rds-options-files/", stock, "_options.csv"), file = file_name)
+  df <- data.table::fread(file_name)
+
+  bucket_name <- paste0("rds-options-files/", stock)
   region_name <- Sys.getenv("AWS_S3_REGION")
 
-  df <- readRDS(paste0(here(), "/data/options/", stock, ".RDS")) %>%
-    mutate(mid = (.data$bid + .data$ask) / 2)
+  aws.s3::put_bucket(bucket_name)
 
   tmp <- tempfile()
   on.exit(unlink(tmp))
-  write.csv(df, file = tmp, row.names = FALSE)
-  # S3 bucket must already exist at this point
-  put_object(tmp,
+  readr::write_csv(df, path = tmp)
+
+  aws.s3::put_object(tmp,
              object = file_name,
              bucket = bucket_name,
              region = region_name)
