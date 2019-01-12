e5a06eb0262a8e65d3db02438afbdcb5361e971d
Jason <jason@themechanicalbear.com>
2019-01-12 09:26:37 -0700

    move to athena, remove .data$ references to pass build check

--- R/open_leg.R
+++ R/open_leg.R
@@ -8,59 +8,39 @@
 #' }
 #'
 #' @import dplyr
+#' @importFrom rlang .data
 #'
-#' @param data table from redshift not yet collected
+#' @param conn AWS Athena connection
+#' @param stock character string of stock symbol
 #' @param put_call is this a put or call option we are opening?
 #' @param direction credit or debit (short or long trade)
 #' @param tar_delta target delta for the option
-#' @param tar_buffer percent the strike should be away from ATM
+#' @param tar_dte target number of days to expiration at entry
 #'
 #' @export
 #'
 #' @return A dataframe of filtered opening option trades to be used in further
 #' analysis.
 #'
-open_leg <- function(data, put_call, direction, tar_delta, tar_buffer) {
-  org <- "1970-01-01"
+
+open_leg <- function(conn, stock, put_call, direction, tar_delta, tar_dte) {
   tp  <- paste0(put_call, "_type")
   s   <- paste0(put_call, "_strike")
-  ds  <- paste0(put_call, "_delta_strike")
+  ds  <- paste0(put_call, "_delta")
   om  <- paste0(put_call, "_open_", direction)
 
-  if (!is.na(tar_delta)) {
-    trades <- data %>%
-      filter(.data$type == put_call) %>%
-      mutate(abs_delta = abs(.data$delta_strike - tar_delta)) %>%
-      group_by(.data$quotedate) %>%
-      filter(.data$m_dte == min(.data$m_dte, na.rm = TRUE)) %>%
-      filter(.data$abs_delta == min(.data$abs_delta, na.rm = TRUE)) %>%
-      ungroup() %>%
-      collect() %>%
-      mutate(!!tp := .data$type,
-             !!s := .data$strike,
-             !!ds := .data$delta_strike,
-             !!om := .data$mid,
-             quotedate = as.Date(.data$quotedate, origin = org),
-             expiration = as.Date(.data$expiration, origin = org))
-  }
-
-  if (!is.na(tar_buffer)) {
-    trades <- data %>%
-      filter(.data$type == put_call) %>%
-      filter(.data$strike < .data$close_price) %>%
-      group_by(.data$quotedate) %>%
-      filter(.data$m_dte == min(.data$m_dte, na.rm = TRUE)) %>%
-      mutate(strike_buff = (.data$close_price - .data$strike) / .data$close_price) %>%
-      filter(.data$strike_buff >= tar_buffer) %>%
-      filter(.data$strike == max(.data$strike, na.rm = TRUE)) %>%
-      ungroup() %>%
-      collect() %>%
-      mutate(!!tp := .data$type,
-             !!s := .data$strike,
-             !!ds := .data$delta_strike,
-             !!om := .data$mid,
-             quotedate = as.Date(.data$quotedate, origin = org),
-             expiration = as.Date(.data$expiration, origin = org))
-  }
-  trades
+  conn %>%
+    tbl(stock) %>%
+    filter(type == put_call) %>%
+    mutate(abs_delta = abs(delta - tar_delta),
+           m_dte = abs(dte - tar_dte)) %>%
+    group_by(quotedate) %>%
+    filter(m_dte == min(m_dte, na.rm = TRUE)) %>%
+    filter(abs_delta == min(abs_delta, na.rm = TRUE)) %>%
+    ungroup() %>%
+    collect() %>%
+    mutate(!!tp := type,
+           !!s := strike,
+           !!ds := delta,
+           !!om := mid)
 }
